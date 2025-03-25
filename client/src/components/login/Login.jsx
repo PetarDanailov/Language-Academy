8ff6f9b4fdaa30ba4d4769bf8d8dcbd7876367ef
PetarDanailov <danailovvpetar@gmail.com>
2025-03-25 19:15:49 +0200

    Made login functional

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,4 +1,26 @@
+import { useNavigate } from "react-router";
+import { useLogin } from "../../api/authApi";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+
 export default function Login(){
+  const navigate = useNavigate();
+  const login = useLogin();
+  const {setUser} = useContext(UserContext);
+  const loginHandler = async (formData) =>{
+    const {email,password} = Object.fromEntries(formData);
+    if(!email || !password){
+      return alert("Please fill all the required fields");
+    }
+    try{
+      const result = await login(email,password);
+      setUser(result);
+      navigate("/courses")
+    }
+    catch(err){
+      alert("Login failed please try again.")
+    }
+  }
   return(
     <div className="login-container">
     <div className="quote-box">
@@ -10,18 +32,18 @@ export default function Login(){
 
     <div className="login-content">
       <div className="login-box">
-        <form id="loginForm" method="post">
+        <form id="loginForm" action={loginHandler}>
           <h1 className="login-title">Login</h1>
           <hr />
           <div className="login-error" role="alert"></div>
           <div className="containers">
             <label className="login-label">Email</label>
-            <input type="email" className="login-input" placeholder="name@example.com" />
+            <input type="email" className="login-input" name="email" placeholder="name@example.com" />
             <span className="login-error"></span>
           </div>
           <div className="containers">
             <label className="login-label">Password</label>
-            <input type="password" className="login-input" placeholder="Password" />
+            <input type="password" className="login-input" name="password" placeholder="Password" />
             <span className="login-error"></span>
           </div>
           <button id="loginSubmit" type="submit" className="login-button">
